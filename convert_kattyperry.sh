987f5ac9e02f069fd8c866c718f786dd21557b5e
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2019-01-16 02:24:35 +0800

    katty perry stuffs

---
+++ convert_kattyperry.sh
@@ -0,0 +1,1 @@
+python audio_to_midi_melodia.py ./fireworks.wav ./fireworks.mid 60 --smooth 0.25 --minduration 0.1 --jams
---
+++ download_kattyperry.sh
@@ -0,0 +1,1 @@
+youtube-dl -x --audio-format wav https://www.youtube.com/watch?v=QGJuMBdaqIw --output fireworks.wav
